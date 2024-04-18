cb7ca971bd32d686627ea952fd7e963485f9753f
goldfinchmax <goldfinch.web@gmail.com>
2024-04-18 22:51:44 +0300

    add homework lesson 2.1

---
+++ lesson_2/homework_2.1.py
@@ -0,0 +1,22 @@
+x = input ("Вееди 4-х значне число, наприклад 1234:\n")
+#print(x)
+i = int(x)
+#print(type(x))
+#print(type(i))
+# cпосіб за допомогою //
+print(i//1000)
+print(i%1000//100)
+print((i%1000%100)//10)
+print((i%1000%100%10)//1)
+#b = i%1000
+#print(b)
+#y = b//100
+#print(y)
+#z = b%100
+#print(z)
+#r = z//10
+#print(r)
+#c = z%10
+#print(c)
+#d = c//10
+#print(d)

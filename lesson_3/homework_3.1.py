18005cf95300b9fd2ec8bcdbef7536b2814b21c4
goldfinchmax <goldfinch.web@gmail.com>
2024-04-25 12:55:36 +0300

    homework_3.1 method 2 (without exit)

--- lesson_3/homework_3.1.py
+++ lesson_3/homework_3.1.py
@@ -4,17 +4,17 @@ y = int(input("введи число 2: \n "))
 
 if diya == '+':
     z = x + y
+    print(z)
 elif diya == '-':
     z = x - y
+    print(z)
 elif diya == '*':
     z = x * y
+    print(z)
 elif diya == '/' and y != 0:
     z = x / y
+    print(z)
 elif diya not in ['+', '-', '*', '/']:
     print("Операція не підтримується, введи іншу операцію, одну з вказаних (+, -, *, /)")
-    exit()
 else:
     print("Введи інше число, на 0 ділити не можна")
-    exit()
-
-print(z)

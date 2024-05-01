0ae6c9b09aa5d56b315bbbc4fc45170bcc736883
goldfinchmax <goldfinch.web@gmail.com>
2024-05-01 13:42:57 +0300

    add homework_5.2 question

--- lesson_5/homework_5.2.py
+++ lesson_5/homework_5.2.py
@@ -2,9 +2,9 @@ import string
 
 text = input("Введи щось, що буде 'хештегом', але не більше 140 символів: \n ")
 title_text = text.title()
-text_exclusion = title_text
+#text_exclusion = title_text
 for char in string.punctuation + string.whitespace:
-    text_exclusion = text_exclusion.replace(char, '')
+    text_exclusion = title_text.replace(char, '')
 print(text_exclusion.count(''))
 result = text_exclusion[:139]
 

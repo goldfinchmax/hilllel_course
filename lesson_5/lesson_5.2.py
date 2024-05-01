5a102fe77a439412a5432fa367394e52202223b2
goldfinchmax <goldfinch.web@gmail.com>
2024-05-01 13:34:32 +0300

    add homework_5.2

---
+++ lesson_5/lesson_5.2.py
@@ -0,0 +1,14 @@
+#email = input('Enter your email: ')
+#print(email)
+#print(email.strip()) # прибтирає пробіли і з права і з ліва
+#print(email.lstrip())
+
+string = "I like dogs and cats"
+array = string.split(" ")
+array = string.split(", ")
+print(array)
+
+
+#joined_string = " ".join(array) #те що в лапках - буде роздільником
+
+#print(joined_string.fin)

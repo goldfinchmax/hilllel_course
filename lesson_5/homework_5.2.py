5781ff5397ce7388b5f79e1a094354cbd550c8f6
goldfinchmax <goldfinch.web@gmail.com>
2024-05-01 13:34:08 +0300

    add homework_5.1 моє бачення

---
+++ lesson_5/homework_5.2.py
@@ -0,0 +1,17 @@
+import string
+
+text = input("Введи щось, що буде 'хештегом', але не більше 140 символів: \n ")
+title_text = text.title()
+text_exclusion = title_text
+for char in string.punctuation + string.whitespace:
+    text_exclusion = text_exclusion.replace(char, '')
+print(text_exclusion.count(''))
+result = text_exclusion[:139]
+
+
+print("Оригінальний текст:", text)
+print("Текст з належними великими літерами:", title_text)
+print("Текст без пунктуації:", text_exclusion)
+print(result)
+print(result.count(''))
+

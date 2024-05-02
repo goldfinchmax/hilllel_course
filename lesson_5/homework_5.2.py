24983b6496cde8d148041f374456d7d89dbd92d1
goldfinchmax <goldfinch.web@gmail.com>
2024-05-02 15:35:52 +0300

    new_push_to_main

--- lesson_5/homework_5.2.py
+++ lesson_5/homework_5.2.py
@@ -2,16 +2,18 @@ import string
 
 text = input("Введи щось, що буде 'хештегом', але не більше 140 символів: \n ")
 title_text = text.title()
-#text_exclusion = title_text
+text_exclusion = title_text
 for char in string.punctuation + string.whitespace:
-    text_exclusion = title_text.replace(char, '')
+    text_exclusion = text_exclusion.replace(char, '')
 print(text_exclusion.count(''))
-result = text_exclusion[:139]
+text_exclusion_hash = '#' + text_exclusion
+result = text_exclusion_hash[:139]
 
 
 print("Оригінальний текст:", text)
 print("Текст з належними великими літерами:", title_text)
 print("Текст без пунктуації:", text_exclusion)
+print(text_exclusion_hash)
 print(result)
 print(result.count(''))
 

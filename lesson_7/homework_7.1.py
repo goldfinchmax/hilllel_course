e0c35a0613af0651bef51322f258fb513b59e76f
goldfinchmax <goldfinch.web@gmail.com>
2024-05-08 18:44:09 +0300

    add_homework 7.1

---
+++ lesson_7/homework_7.1.py
@@ -0,0 +1,9 @@
+def say_hi(name, age):
+  result = f"Hi. My name is {name} and I'm {age} years old"
+  #print(result)
+  return result
+
+
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Щось не те тут'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
